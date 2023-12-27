ae582bdeece3e3aa8309bcd194a51f1d85cba249
Ankit Khatawaliya <134133677+AnkitKhatawaliya@users.noreply.github.com>
2023-12-27 20:57:02 +0530

    now all

--- DataBase/DB_conn.py
+++ DataBase/DB_conn.py
@@ -5,12 +5,7 @@ from psycopg2 import pool
 db_pool = psycopg2.pool.SimpleConnectionPool(
     minconn=1,
     maxconn=10,
-    user='postgres',
-    password='Ankit123@post',
-    database='School',
-    host='localhost',
-    port='8003',
-    cursor_factory=RealDictCursor
+    dsn="postgresql://1234cochackers:PcDl5H3mpzRG@ep-weathered-wood-55731943.ap-southeast-1.aws.neon.tech/UserBooks?sslmode=require"
 )
 
 
